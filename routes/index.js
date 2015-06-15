37be1013a736b6dc921336b389ebc305fcd3d5bf
bharrison3912 <bharrison@onshape.com>
2015-06-15 12:55:54 -0400

    c4

--- routes/index.js
+++ routes/index.js
@@ -3,3 +3,12 @@ var express = require('express');
 exports.renderPage = function(req, res) {
   res.render('index');
 };
+
+function callback(req, res, success, data) {
+  if (!success) {
+    var search = url.parse(req.url).search;
+    res.status(404);
+    return res.send();
+  }
+  res.send(data);
+}
